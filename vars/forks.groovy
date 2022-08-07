b462d35349fde933ab28efff2038c5877827335c
Flame0n <lexainb3@gmail.comm>
2022-08-07 20:33:08 +0200

    wip

--- vars/forks.groovy
+++ vars/forks.groovy
@@ -34,5 +34,5 @@ def executeStages(String repo="https://github.com/ROCmSoftwarePlatform/tensorflo
 
 def call(Map parameters) {
     println(params.executionType)
-    executeStages(String repo="https://github.com/ROCmSoftwarePlatform/tensorflow-upstream/", String branch="master", String credentialsId="Token", String executionType="cpu")
+    executeStages(repo="https://github.com/ROCmSoftwarePlatform/tensorflow-upstream/", branch="master", credentialsId="Token", executionType="cpu")
 }
