b25f2247e44acea0960ceacb1e5e5e5cba066903
Flame0n <lexainb3@gmail.comm>
2022-08-09 08:28:38 +0200

    Up

--- vars/forks.groovy
+++ vars/forks.groovy
@@ -1,34 +1,37 @@
-def executeCommand(String parameter) {
+def executeCommand(String executionType) {
     println("Build executed")
     sh """
-        tensorflow/tools/ci_build/ci_build.sh ROCM ./tensorflow/tools/ci_build/linux/rocm/run_${parameter}.sh
+        tensorflow/tools/ci_build/ci_build.sh ROCM ./tensorflow/tools/ci_build/linux/rocm/run_${executionType}.sh
     """
 }
 
-def executeStages(String repo="https://github.com/ROCmSoftwarePlatform/tensorflow-upstream/", String branch="master", String credentialsId="Token", String executionType="cpu"){
-    node("rocm"){
-        stage("Set up Docker"){
-            restartDocker()
-        }
-        stage("Clone project"){
-            checkout(
-                [
-                    $class: 'GitSCM',
-                    userRemoteConfigs: [[url: repo, credentialsId:credentialsId]],
-                    branches: [[name: branch]]
-                ]
-            )
-        }
-        stage("Build execution"){
-            executeCommand(executionType)
+def executeStages(String repo="https://github.com/ROCmSoftwarePlatform/tensorflow-upstream/", String branch="master", String credentialsId="Token"){
+    Map stages = [:]
+    List listOfStages = ["run_cpu", "run_gpu_multi", "run_gpu_single", "rocm_ci_sanity"]
+
+    listOfStages.each() {
+        stages[it] = {
+            def label = it == "run_gpu_multi" ? "rocm&&multi_gpu" : "rocm"
+            node(label){
+                restartDocker()
+                checkout(
+                    [
+                        $class: 'GitSCM',
+                        userRemoteConfigs: [[url: repo, credentialsId:credentialsId]],
+                        branches: [[name: branch]]
+                    ]
+                )
+                executeCommand(executionType)
+            }
         }
     }
+
+    parallel stages
 }
 
 def call(Map parameters) {
     try {
-        
-        executeStages(parameters["repo"], parameters["branch"], parameters["credentials_id"], parameters["executionType"])
+        executeStages()
         currentBuild.result = "SUCCESS"
         currentBuild.description = "<b>Success</b><br/>"
     } catch(e) {
