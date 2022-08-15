f7608b6503ad1b445d2057d8feb7fe19289f0a34
Flame0n <lexainb3@gmail.comm>
2022-08-15 09:45:31 +0200

    Up

--- jenkins/tensorflow/buildonly/build-only.groovy
+++ jenkins/tensorflow/buildonly/build-only.groovy
@@ -1,18 +1,35 @@
 def executeBuild(){
-    sh """#!/bin/bash
-        sed -i 's|ROCM_EXTRA_PARAMS="--device=/dev/kfd --device=/dev/dri --group-add video |ROCM_EXTRA_PARAMS="|g' tensorflow/tools/ci_build/ci_build.sh
-        BUILD_SCRIPT=build_rocm_python3
-        cat <<EOF > \$BUILD_SCRIPT
-        set -eux
-        ROCM_PATH=/opt/rocm-5.1.0
-        yes "" | ROCM_PATH=\$ROCM_PATH TF_NEED_ROCM=1 PYTHON_BIN_PATH=/usr/bin/python3 ./configure
-        cat .tf_configure.bazelrc
-        bazel build --config=rocm //tensorflow/tools/pip_package:build_pip_package --verbose_failures
-        bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-        EOF
-        chmod a+x ./\$BUILD_SCRIPT
-        tensorflow/tools/ci_build/ci_build.sh ROCM ./\$BUILD_SCRIPT
+    sh """#!/bin/bash -x
 
+        # Build-only mod:  remove docker's use of kfd and dri devices
+        sed -i 's|ROCM_EXTRA_PARAMS="--device=/dev/kfd --device=/dev/dri --group-add video |ROCM_EXTRA_PARAMS="|g' tensorflow/tools/ci_build/ci_build.sh
+
+
+        BUILD_SCRIPT=build_rocm_python3
+
+
+        cat <<EOF > \$BUILD_SCRIPT
+        #!/bin/bash
+
+        set -eux
+
+        ROCM_PATH=/opt/rocm-5.1.0
+
+        yes "" | ROCM_PATH=\$ROCM_PATH TF_NEED_ROCM=1 PYTHON_BIN_PATH=/usr/bin/python3 ./configure
+
+        cat .tf_configure.bazelrc
+
+        bazel build --config=rocm //tensorflow/tools/pip_package:build_pip_package --verbose_failures
+
+        bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+
+        EOF
+
+
+        chmod a+x ./\$BUILD_SCRIPT
+
+
+        tensorflow/tools/ci_build/ci_build.sh ROCM ./\$BUILD_SCRIPT
     """
 }
 
@@ -28,7 +45,7 @@ def checkoutProject(String repo, String branch){
 
 pipeline {
     agent {
-        node { label 'ixt-rack-125' }
+        node { label 'build-only' }
     }
     options {
         timeout(time: 3, unit: 'HOURS')
