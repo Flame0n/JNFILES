395a8bb0972513acfa34bd4976a69f3666de7527
Flame0n <lexainb3@gmail.comm>
2022-08-18 13:33:48 +0200

    Fixed archieving arts

--- jenkins/pytorch/apex_nightly.groovy
+++ jenkins/pytorch/apex_nightly.groovy
@@ -134,8 +134,8 @@ pipeline {
     }
     post {
         always {
-            archiveArtifacts artifacts: "*.log", fingerprint: true
-            archiveArtifacts artifacts: "wheels/*.whl", fingerprint: true
+            archiveArtifacts artifacts: "*.log", allowEmptyArchive: true
+            archiveArtifacts artifacts: "wheels/*.whl", allowEmptyArchive: true
             emailext to: "lexainb3@gmail.com",
                 subject: "jenkins build:${currentBuild.currentResult}: ${env.JOB_NAME}",
                 body: "${currentBuild.currentResult}: Job ${env.JOB_NAME}\nMore Info can be found here: ${env.BUILD_URL}"
